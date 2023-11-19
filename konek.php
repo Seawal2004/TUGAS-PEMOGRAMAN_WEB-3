ad844ff7926923169e8ffee205b6c9cbf202b4cb
Seawal2004 <147905575+Seawal2004@users.noreply.github.com>
2023-11-19 20:49:16 +0700

    Add files via upload

---
+++ konek.php
@@ -0,0 +1,18 @@
+<?php
+// Koneksi ke database
+$servername = "localhost"; // Ganti dengan nama server Anda
+$username = "root"; // Ganti dengan username database Anda
+$password = ""; // Ganti dengan password database Anda
+$dbname = "tugas_web";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Cek koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+
+
+?>
+
+
